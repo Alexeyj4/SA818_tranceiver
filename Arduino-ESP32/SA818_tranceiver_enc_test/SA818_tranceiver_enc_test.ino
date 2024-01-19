24294bb4e6cc32f20ebf1ebac69d6eb8f7530f59
Alexey <alexeyj4@yandex.ru>
2024-01-19 22:43:07 +0300

    ..

---
+++ Arduino-ESP32/SA818_tranceiver_enc_test/SA818_tranceiver_enc_test.ino
@@ -0,0 +1,41 @@
+#include <Oled1_3.h>
+#include <Ble.h>
+#include <ESP32Encoder.h>
+const gpio_num_t btn_pin=GPIO_NUM_25; //иначе не работает выход из спящего режима, если просто int 19 указать //encoder Button pin
+const int OLEDVCC_pin=4;
+const int enc_a_pin=27; //encoder A pin
+const int enc_b_pin=26; //encoder B pin
+
+int enc_oldPosition  = 0; //encoder old position
+
+Oled oled;
+Ble ble;
+ESP32Encoder enc;
+
+void setup() { 
+  pinMode(OLEDVCC_pin, OUTPUT);
+  pinMode(enc_a_pin, INPUT_PULLUP);//A
+  pinMode(enc_b_pin, INPUT_PULLUP);//B
+  pinMode(btn_pin,INPUT_PULLUP);
+  digitalWrite(OLEDVCC_pin,1);
+  Serial.begin(115200);   
+  ble.begin();
+  oled.begin();  
+  enc.attachFullQuad(enc_a_pin, enc_b_pin);
+  enc.clearCount();
+}
+
+void loop() {
+  if(ble.recvd()!=""){
+    oled.print(1,ble.recvd());
+    ble.clr();
+  }
+  oled.update();  
+  delay(50);
+  if(digitalRead(btn_pin)==0){
+    ble.send("Нажата кнопка");    
+    delay(1000);
+  }       
+oled.print(2,String((int32_t)(floor(enc.getCount()/4)))); 
+
+}
