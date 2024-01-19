7d8b106ab4cebe05b26c4a9ad8bf2dbb60d95f1a
Alexey <alexeyj4@yandex.ru>
2024-01-19 23:33:07 +0300

    ююю

--- Arduino-ESP32/SA818_tranceiver_test_uart/SA818_tranceiver_test_uart.ino
+++ Arduino-ESP32/SA818_tranceiver_test_uart/SA818_tranceiver_test_uart.ino
@@ -5,9 +5,19 @@ void setup() {
   digitalWrite(19,1);
   Serial.begin(9600);
   Serial2.begin(9600, SERIAL_8N1);
+  delay(100);
 }
 
 void loop() {
+Serial.println("sleep...");
+delay(2000);
+gpio_wakeup_enable(GPIO_NUM_25,GPIO_INTR_LOW_LEVEL);
+esp_sleep_enable_gpio_wakeup();
+esp_light_sleep_start();
+Serial.println("hello");
+delay(5000);
+
+  
 if(Serial.available()){
   Serial2.print(Serial.read());
 }
@@ -16,25 +26,25 @@ if(Serial2.available()){
   Serial.write(Serial2.read());
 }   
 
-if(digitalRead(25)==0){
-  Serial2.print("RSSI?");
-  Serial2.write(13);
-  Serial2.write(10);
-  digitalWrite(19,0);//ptt on
-  pinMode(32, OUTPUT);//rx1
-  for(int i=0;i<5000;i++){
-    digitalWrite(32,1);
-    delay(2);
-    digitalWrite(32,0);
-    delay(2);
-  }  
-  pinMode(32, INPUT);//rx1
-  digitalWrite(19,1);//ptt off  
-}
-int l=(int(analogRead(32)/50));
-//Serial.println(l);
-for(int i=0;i<l;i++){
-  Serial.print("*");  
-}
-Serial.println();
+//if(digitalRead(25)==0){
+//  Serial2.print("RSSI?");
+//  Serial2.write(13);
+//  Serial2.write(10);
+//  digitalWrite(19,0);//ptt on
+//  pinMode(32, OUTPUT);//rx1
+//  for(int i=0;i<5000;i++){
+//    digitalWrite(32,1);
+//    delay(2);
+//    digitalWrite(32,0);
+//    delay(2);
+//  }  
+//  pinMode(32, INPUT);//rx1
+//  digitalWrite(19,1);//ptt off  
+//}
+//int l=(int(analogRead(32)/50));
+////Serial.println(l);
+//for(int i=0;i<l;i++){
+//  Serial.print("*");  
+//}
+//Serial.println();
 }
